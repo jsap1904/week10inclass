1e649aa240a9dea90c7119d1fae8627cacc0b60b
jsap1904 <sapinoso.j@gmail.com>
2017-03-21 19:24:01 -0700

    update

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// var newDiv = document.createElement('div')
+// var text = document.createTextNode('sup')
+// var body = document.getElementById('headers')
+// var newImg = document.createElement('img')
+// //send new div to body
+// body.appendChild(newDiv)
+// //put text into newDiv
+// newDiv.appendChild(text)
+// //give class of 'cyan' to newDiv
+// newDiv.className = "cyan"
+// //send newImg into newDiv
+// newDiv.appendChild(newImg)
+// //give newImg a source to a img
+// newImg.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/7/79/2010-brown-bear.jpg")
+
+	
+
+// var boolean = true
+// 	console.log(boolean.length)
+
+// var string = "i'm a starboy"
+// 	console.log(string.length)
+
+// var num = 1,2,3
+// 	console.log(num.length)
+
+// var array = [1,true, "yay"]
+// 	console.log(array.length)
+
+// var object = {
+// 	pokemon: "pikachu",
+// 	car: toyota,
+// 	age: 4;
+// },{
+// 	pokemon: "charmander",
+// 	car: honda,
+// 	age 6;
+// }
+// 	console.log(object.length)
+
+var poke = ['tuna', 'salmon', 'mackeral', 'ikura', 'uni', 'hamachi', 'tamago']
+	
+	poke.pop()
+
+	console.log(poke)
